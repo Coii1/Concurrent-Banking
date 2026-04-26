9b39113a5aa9ae7e7507786c3622dad18b67ab3a
Coi <ctoyco1@up.edu.ph>
2026-04-26 10:47:44 +0800

    ux: print operatation details at current tick

--- bankdb/src/transaction.c
+++ bankdb/src/transaction.c
@@ -3,6 +3,25 @@
 #include "timer.h"
 #include <stdio.h>
 
+static void print_operation(Operation* op) {
+    switch (op->type) {
+        case OP_DEPOSIT:
+            printf("DEPOSIT account=%d amount=%d\n", op->account_id, op->amount_centavos);
+            break;
+        case OP_WITHDRAW:
+            printf("WITHDRAW account=%d amount=%d\n", op->account_id, op->amount_centavos);
+            break;
+        case OP_TRANSFER:
+            printf("TRANSFER from=%d to=%d amount=%d\n", op->account_id, op->target_account, op->amount_centavos);
+            break;
+        case OP_BALANCE:
+            printf("BALANCE account=%d\n", op->account_id);
+            break;
+        default:
+            printf("UNKNOWN OPERATION\n");
+    }
+}
+
 void* execute_transaction(void* arg) {
 
     Transaction* tx = (Transaction*)arg;
@@ -15,6 +34,8 @@ void* execute_transaction(void* arg) {
 
         // Placeholder for now: just print progress
         printf("T%d reached op %d at tick %d\n", tx->tx_id, i, target_tick);
+        print_operation(&tx->ops[i]);
+
     }
 
     return NULL;
