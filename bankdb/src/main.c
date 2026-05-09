aa761c2e24da22dd1d4d1f3a8fab8a5d4ba350c3
Andrea Laserna <aalaserna@up.edu.ph>
2026-05-09 16:23:34 +0800

    refactor: made parsing easier, added timer_stop and printing final report

--- bankdb/src/main.c
+++ bankdb/src/main.c
@@ -5,8 +5,10 @@
 #include "transaction.h"
 #include "timer.h"
 #include "utils.h"
+#include "metrics.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
 
 static const char* op_name(OpType t) {
     switch (t) {
@@ -60,30 +62,79 @@ static void print_loaded_accounts(Bank* bank) {
     }
 }
 
+// Configuration variables to store simulation settings
+char *accounts_path = NULL;
+char *trace_path = NULL;
+int tick_interval_ms = 100; // Default from manual
+char *deadlock_strategy = "prevention"; // Default
+
+// Use getopt to read input from terminal and assign to config variables
+void parse_args(int argc, char* argv[]) {
+    static struct option long_options[] = {
+        {"accounts", required_argument, 0, 'a'},
+        {"trace",    required_argument, 0, 't'},
+        {"deadlock", required_argument, 0, 'd'},
+        {"tick-ms",  required_argument, 0, 'm'},
+        {0, 0, 0, 0}
+    };
+
+    int opt;
+    // getopt_long : handle flags without manually looping through argv[]
+    while ((opt = getopt_long(argc, argv, "", long_options, NULL)) != -1) {
+        switch (opt) {
+            case 'a': accounts_path = optarg; break;
+            case 't': trace_path = optarg; break;
+            case 'd': deadlock_strategy = optarg; break;
+            case 'm': tick_interval_ms = atoi(optarg); break;
+        }
+    }
+
+    if (!accounts_path || !trace_path) {
+        fprintf(stderr, "Usage: %s --accounts=FILE --trace=FILE [--deadlock=prevention|detection] [--tick-ms=N]\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+}
+
 Bank* bank = NULL; // Global bank instance for now, remove when buffer pool is implemented
 
 int main(int argc, char* argv[]) {
 
     //parse CLI args for accounts file and trace file paths.
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s <accounts_file> <trace_file>\n", argv[0]);   
-        exit(EXIT_FAILURE);
-    }
-    char *accounts_path = argv[1];
-    char *trace_path = argv[2];
-    int tick_interval_ms = 1; // can make this configurable later if needed; hardcoded to 1 second per tick for now.
+    // if (argc != 3) {
+    //     fprintf(stderr, "Usage: %s <accounts_file> <trace_file>\n", argv[0]);   
+    //     exit(EXIT_FAILURE);
+    // }
+    // char *accounts_path = argv[1];
+    // char *trace_path = argv[2];
+    // int tick_interval_ms = 1; // can make this configurable later if needed; hardcoded to 1 second per tick for now.
     
+    parse_args(argc, argv);
 
     //initialize bank, accounts, and locks.  
     bank = create_bank(); 
-    //declare transactions array in main for now. NOT SURE if it should be added in the bank struct
-    Transaction *txs[MAX_TRANSACTIONS];
-    
-    if (load_accounts_file(bank, accounts_path)){
-        fprintf(stderr, "Failed to load accounts from %s\n", argv[1]);
+
+    // Use the variable populated by parse_args to populate bank struct
+    if (load_accounts_file(bank, accounts_path)) {
+        fprintf(stderr, "Failed to load accounts from %s\n", accounts_path);
         destroy_bank(bank);
         return 1;
     }
+
+
+    int initial_sum = 0;
+    for (int i = 0; i < bank->num_accounts; i++) {
+        initial_sum += bank->accounts[i].balance_centavos;
+    }
+
+    //declare transactions array in main for now. NOT SURE if it should be added in the bank struct
+    Transaction *txs[MAX_TRANSACTIONS];
+    
+    // File already loaded above
+    // if (load_accounts_file(bank, accounts_path)){
+    //     fprintf(stderr, "Failed to load accounts from %s\n", argv[1]);
+    //     destroy_bank(bank);
+    //     return 1;
+    // }
     // load transaction trace
     int txs_count = load_transactions_file(trace_path, txs);
     if (txs_count < 0) {
@@ -122,29 +173,34 @@ int main(int argc, char* argv[]) {
         }
     }
 
+    // Wait for all transactions to finish
     for (int i = 0; i < txs_count; i++) {
         if (pthread_join(txs[i]->thread, NULL) != 0) {
             fprintf(stderr, "Failed to join thread for transaction T%d\n", txs[i]->tx_id);
         }
     }
     
+    // Set all_transactions_done to True to stop timer_thread's infinite while true loop
+    timer_stop();
 
+    // Wait for timer thread to exit
     if (pthread_join(timer_tid, NULL) != 0) {
         perror("Failed to join thread");
     }
+
+    // Destroy mutex and condition variable
     timer_destroy();
 
-
-
-
-    // TODO:  create transaction threads, and execute transactions
-
+    // Create transaction threads, and execute transactions
     for (int i = 0; i < MAX_TRANSACTIONS; i++) {
         if (txs[i] != NULL) {
             free(txs[i]);
         }
     }
+
+    print_final_report(bank, txs, txs_count, initial_sum);
+
     destroy_bank(bank);
-    return 0; 
+    return 0;
 }
 
